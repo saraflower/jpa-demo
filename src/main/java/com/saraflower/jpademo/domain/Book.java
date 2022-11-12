1d0ce53a1296c57db2f2dd199b0cbeb883230c83
Sara Pablo <lunielledev@gmail.com>
2022-11-12 13:56:25 +0100

    Initial commit

---
+++ src/main/java/com/saraflower/jpademo/domain/Book.java
@@ -0,0 +1,28 @@
+package com.saraflower.jpademo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String title;
+    @NonNull
+    private String isbn;
+    @NonNull
+    private String publisher;
+    @NonNull
+    private Long price;
+}
