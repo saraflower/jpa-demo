b6b1937d682a1c4acde2e275495c4bb2d5c29934
Sara Pablo <lunielledev@gmail.com>
2022-12-20 01:49:01 +0100

    mysql config

--- src/main/java/com/saraflower/jpademo/bootstrap/DataInitializer.java
+++ src/main/java/com/saraflower/jpademo/bootstrap/DataInitializer.java
@@ -14,10 +14,12 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Book book1 = new Book("The Great Book I", "1", "The Great Publisher", 500L);
-        Book book2 = new Book("The Great Book II", "2", "The Great Publisher", 550L);
+        if(bookRepo.count() == 0) {
+            Book book1 = new Book("The Great Book I", "1", "The Great Publisher", 500L);
+            Book book2 = new Book("The Great Book II", "2", "The Great Publisher", 550L);
 
-        bookRepo.save(book1);
-        bookRepo.save(book2);
+            bookRepo.save(book1);
+            bookRepo.save(book2);
+        }
     }
 }
